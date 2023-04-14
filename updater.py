87dd288d4d56b4fd027d17444905d653c0e9f157
Gregory <re7gog@gmail.com>
2023-04-14 18:05:30 +0300

    New GitHub filter and download logic

--- updater.py
+++ updater.py
@@ -1,109 +1,122 @@
-from concurrent.futures import ThreadPoolExecutor
 from glob import glob
 from multiprocessing import Process
 from os import path, remove
-from urllib.parse import unquote
+
+from requests import get
 
 try:
     import tomllib
 except ModuleNotFoundError:
     import tomli as tomllib
-from requests import get
 
 
 class Updater:
     def __init__(self, settings):
-        self.settings = settings
+        self.sett = settings
+        self.downloads = set()
 
-    def _make_filepath(self, filename, tag, mod_id):
+    def make_filepath(self, filename, tag, mod_id):
         dot_pos = filename.rfind('.')
         name = filename[:dot_pos] + "-" + tag + str(mod_id)
-        filepath = self.settings['mods_path'] + name + filename[dot_pos:]
+        filepath = self.sett['mods_path'] + name + filename[dot_pos:]
         return filepath, filename[dot_pos:]
 
+    def _remove_old_file(self, filetype, tag, mod_id):
+        old_file = glob(self.make_filepath("*" + filetype, tag, mod_id)[0])
+        if old_file:
+            remove(old_file[0])
+
     @staticmethod
-    def _downloader(args):
-        file_stream = get(args[0], stream=True)
-        with open(args[1], 'wb+') as file:
+    def _downloader(url, filename):
+        file_stream = get(url, stream=True)
+        with open(filename, 'wb+') as file:
             for chunk in file_stream.iter_content(1024):
                 file.write(chunk)
 
-    def download(self, downloads):
-        with ThreadPoolExecutor(max_workers=len(downloads)) as executor:
-            executor.map(self._downloader, downloads)
+    def download(self, *args):
+        proc = Process(target=self._downloader, args=args)
+        proc.start()
+        self.downloads.add(proc)
+
+    def wait_downloads(self):
+        for dwnld in self.downloads:
+            dwnld.join()
 
 
 class CurseForgeUpdater(Updater):
     def _get_mod_info(self, mod_id):
-        params = {'gameVersion': self.settings['game_ver'], 'pageSize': 1}
+        params = {'gameVersion': self.sett['game_ver'], 'pageSize': 1}
         req = get(f'https://api.curseforge.com/v1/mods/{mod_id}/files',
                   headers=self.HEADERS, params=params)
         return req.json()['data'][0]
 
-    def _url_if_no_cdn(self, mod_id, file_id):
+    def _make_url(self, mod_id, file_info):
+        if file_info['downloadUrl']:
+            return file_info['downloadUrl']
         req = get(f'https://api.curseforge.com/v1/mods/{mod_id}',
                   headers=self.HEADERS)
         website = req.json()['data']['links']['websiteUrl']
-        return f"{website}/download/{file_id}/file"
+        return f"{website}/download/{file_info['id']}/file"
 
     def __init__(self, settings):
         super().__init__(settings)
         self.HEADERS = {
             'Accept': 'application/json',
-            'x-api-key': self.settings['curseforge']['key']
+            'x-api-key': self.sett['curseforge']['key']
         }
-        downloads = set()
-        for mod_id in self.settings['curseforge']['mods_ids']:
+        for mod_id in self.sett['curseforge']['mods_ids']:
             file_info = self._get_mod_info(mod_id)
-            filepath, filetype = self._make_filepath(
+            filepath, filetype = self.make_filepath(
                 file_info['fileName'], 'c', mod_id)
             if not path.exists(filepath):
-                old_file = glob(self._make_filepath(
-                    "*" + filetype, 'c', mod_id)[0])
-                if old_file:
-                    remove(old_file[0])
-                url = file_info['downloadUrl']
-                if not url:
-                    url = self._url_if_no_cdn(mod_id, file_info['id'])
-                downloads.add((url, filepath))
-        if downloads:
-            self.download(downloads)
+                self._remove_old_file(filetype, 'c', mod_id)
+                url = self._make_url(mod_id, file_info)
+                self.download(url, filepath)
+        self.wait_downloads()
 
 
 class GithubUpdater(Updater):
-    def _check(self, repo, ver):
-        release = not ver['prerelease'] or \
-            ver['prerelease'] and self.settings['github']['use_prereleases']
-        game_ver = not self.settings['github']['repos'][repo][1] or \
-            self.settings['game_ver'] in ver['name']
-        return release and game_ver
+    @staticmethod
+    def _base_checker(rules, rule_tag, check_obj):
+        for rule in filter(lambda i: i[0] == rule_tag, rules):
+            must_be = rule[1] == '+'
+            word_there = rule[2:] in check_obj
+            if word_there != must_be:
+                return False
+        return True
+
+    def _check_release(self, release, rules):
+        prerelease_ok = not release['prerelease'] or \
+            self.sett['github']['use_prereleases']
+        if not prerelease_ok:
+            return False
+        return self._base_checker(rules, 'r', release['name'])
 
     def __init__(self, settings):
         super().__init__(settings)
-        repos = (i[0] for i in self.settings['github']['repos'])
+        repos = (i[0] for i in self.sett['github']['repos'])
         headers = {'Accept': "application/vnd.github+json"}
-        downloads = set()
+        params = {'per_page': 10}
         for repo_i, repo in enumerate(repos):
             repo_info = get(f"https://api.github.com/repos/{repo}/releases",
-                            headers=headers).json()
+                            headers=headers, params=params).json()
             if 'message' in repo_info:
                 print(repo_info['message'])
                 return
-            for ver in repo_info:
-                if self._check(repo_i, ver):
-                    url = ver['assets'][-1]['browser_download_url']
-                    file_name = unquote(url[url.rfind("/") + 1:])
-                    filepath, filetype = self._make_filepath(
-                        file_name, 'g', repo_i)
-                    if not path.exists(filepath):
-                        old_file = glob(self._make_filepath(
-                            "*" + filetype, 'g', repo_i)[0])
-                        if old_file:
-                            remove(old_file[0])
-                        downloads.add((url, filepath))
+            rules = self.sett['github']['repos'][repo_i][1]
+            for release in repo_info:
+                if self._check_release(release, rules):
+                    for asset in release['assets']:
+                        if self._base_checker(rules, 'a', asset['name']):
+                            filepath, filetype = self.make_filepath(
+                                asset['name'], 'g', repo_i)
+                            if not path.exists(filepath):
+                                self._remove_old_file(filetype, 'g', repo_i)
+                                self.download(
+                                    asset['browser_download_url'], filepath)
+                            break
                     break
-        if downloads:
-            self.download(downloads)
+        self.wait_downloads()
 
 
 if __name__ == '__main__':
@@ -111,9 +124,9 @@ if __name__ == '__main__':
         sett = tomllib.loads(sett_file.read())
     if not path.exists(sett['mods_path']):
         raise FileNotFoundError("mods directory doesn't exists")
-    p1 = Process(target=CurseForgeUpdater, args=(sett,))
-    p1.start()
-    p2 = Process(target=GithubUpdater, args=(sett,))
-    p2.start()
-    p2.join()
-    p1.join()
+    modules = CurseForgeUpdater, GithubUpdater
+    procs = (Process(target=m, args=(sett,)) for m in modules)
+    for p in procs:
+        p.start()
+    for p in procs:
+        p.join()
